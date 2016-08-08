e3c67392e6789512e91150ed3f9167534b0914cc
alexanderspace100 <alexanderspace100@gmail.com>
2016-08-08 13:46:06 +0300

    done

---
+++ bin/environment.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+
+Bundler.require
+
+require_relative "../lib/pokemon"
+require_relative "../lib/scraper"
+require_relative "sql_runner"
---
+++ bin/sql_runner.rb
@@ -0,0 +1,19 @@
+class SQLRunner
+  def initialize(db)
+    @db = db
+  end
+
+  def execute_schema_migration_sql
+    sql = File.read('db/schema_migration.sql')
+    execute_sql(sql)
+  end
+
+  def execute_sql(sql)
+    sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
+  end
+
+  def execute_create_hp_column
+    sql = File.read('db/execute_create_hp_column.sql')
+    execute_sql(sql)
+  end
+end
